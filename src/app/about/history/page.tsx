bafbc68664b8507b876b57a41fd8b739ae18933f
Rajaul Karim <56322890+Rajaul2810@users.noreply.github.com>
2026-03-16 15:08:08 +0600

    Update page.tsx

--- src/app/about/history/page.tsx
+++ src/app/about/history/page.tsx
@@ -12,13 +12,13 @@ export default function TwoPdfViewer() {
   ];
 
   return (
-    <div className="max-w-5xl mx-auto p-6 grid md:grid-cols-2 gap-6">
+    <div className="max-w-5xl mx-auto p-6 grid md:grid-cols-2 gap-6 sm:py-24">
       {pdfs.map((pdf, index) => (
         <div
           key={index}
-          className="bg-white border rounded-xl shadow-sm p-6 flex flex-col justify-between"
+          className="bg-white dark:bg-gray-800 border rounded-xl shadow-sm p-6 flex flex-col justify-between"
         >
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl font-semibold text-gray-800 dark:text-white mb-4">
             {pdf.title}
           </h2>
 
@@ -26,7 +26,7 @@ export default function TwoPdfViewer() {
             href={pdf.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-block text-center bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
+            className="inline-block text-center bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
           >
             View PDF
           </a>
